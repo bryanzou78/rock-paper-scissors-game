99a66c48cd72c95306576adcce54f0df497181ae
Bryan Zou <175367072+bryanzou78@users.noreply.github.com>
2024-08-12 13:33:11 -0700

    Add colliding animation and move sections around

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const scissorsBtn = document.getElementById('scissors-btn');
 const userScoreDisplay = document.getElementById('user-score');
 const computerScoreDisplay = document.getElementById('computer-score');
 const results = document.querySelector('.results');
+const userVersus = document.querySelector('.user-versus');
+const computerVersus = document.querySelector('.computer-versus');
 
 let userScore = 0;
 let computerScore = 0;
@@ -24,19 +26,25 @@ function playRound(userChoice, computerChoice) {
         || userChoice === 'scissors' && computerChoice === 'paper') {
             userScore++;
             userScoreDisplay.innerHTML = userScore;
-            results.innerHTML = `Computer chooses ${computerChoice}, you win!`;
+            results.innerHTML = `Computer chooses ${computerChoice}, you win! Play again?`;
         } else if (userChoice === 'rock' && computerChoice === 'paper'
             || userChoice === 'paper' && computerChoice === 'scissors'
             || userChoice === 'scissors' && computerChoice === 'rock') {
                 computerScore++;
                 computerScoreDisplay.innerHTML = computerScore;
-                results.innerHTML = `Computer chooses ${computerChoice}, you lose!`;
+                results.innerHTML = `Computer chooses ${computerChoice}, you lose! Try again?`;
             } else {
-                results.innerHTML = `Computer chooses ${computerChoice}, it's a draw!`;
+                results.innerHTML = `Computer chooses ${computerChoice}, it's a draw! Try again?`;
             }
-        console.log(userScore);
-        console.log(userChoice);
-        console.log(computerChoice);
+
+    userVersus.innerHTML = `<img src='images/${userChoice}.svg'>`;
+    computerVersus.innerHTML = `<img src='images/${computerChoice}.svg'>`;
+
+    userVersus.classList.add('animate-user');
+    setTimeout(() => userVersus.classList.remove('animate-user'), 1200);
+
+    computerVersus.classList.add('animate-computer');
+    setTimeout(() => computerVersus.classList.remove('animate-computer'), 1200);
 }
 
 // 
